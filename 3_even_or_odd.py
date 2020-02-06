f4167b4e321f5a984a0756593541c2d69aa1e494
souvikban16 <59077658+souvikban16@users.noreply.github.com>
2020-02-06 06:10:27 +0530

    fixed again

---
+++ 3_even_or_odd.py
@@ -0,0 +1,2 @@
+n=int(input("Enter the number to check for even or odd="))
+print(("Even","Odd")[n%2!=0])
---
+++ 4_swap_without_third.py
@@ -0,0 +1,4 @@
+a,b=input('Enter the numbers separated by space').split()
+a,b=int(a),int(b)
+a=a+b; b=a-b; a=a-b
+print("a=",a," b=",b)
