2c2c26331558d2b12d6c59a849b1d6828bb9627c
souvikban16 <59077658+souvikban16@users.noreply.github.com>
2020-02-13 00:32:57 +0530

    Create 7_area_of_circle.c

---
+++ 7_area_of_circle.c
@@ -0,0 +1,10 @@
+import <stdio.h>;
+
+void main()
+{
+	printf("Enter the radius of the circle");
+	int c;scanf("%d",&d);
+	float area=0.0;
+	area=3.14*(c*c);
+	printf("The area of the circle is="+area);
+}
