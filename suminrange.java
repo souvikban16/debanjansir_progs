22bbe39375f553c33058bd87f2b711eaeaf27a2f
souvikban16 <59077658+souvikban16@users.noreply.github.com>
2020-02-19 21:46:01 +0530

    eve

--- suminrange.java
+++ suminrange.java
@@ -1,1 +1,19 @@
-import java.util
+import java.util.Scanner;
+
+class suminarange
+{
+	public static void main(String args[])
+	{
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter the range");
+		int r=in.nextInt();
+		int sum=0;
+		for(int i=0;i<=r;i++)
+		{
+			sum+=i;
+		}
+		System.out.println("Sum="+sum);
+
+
+	}
+}
